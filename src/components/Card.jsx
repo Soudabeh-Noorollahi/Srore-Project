9544faf5e95d43b480d6c9c2c47c71ccdefcc1cf
Soudabeh Noorollahi <s.nourollahii@gmail.com>
2025-01-16 17:28:55 +0100

    Update category management with context and useState

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,32 @@
 import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
 import { TbShoppingBagCheck } from "react-icons/tb";
+
 import { shortenText } from "../helper/helper";
+import { useCart } from "../context/CartContext";
 
 import styles from "./Card.module.css";
 
 function Card({ data }) {
   const { id, title, image, price } = data;
 
+  const [state, dispatch] = useCart();
+  console.log(state);
+
+  const clickHandler = () => {
+    dispatch({ type: "REMOVE_ITEM", payload: data });
+  };
+
   return (
     <div className={styles.card}>
       <img src={image} alt={title} />
       <h3>{shortenText(title)}</h3>
-      <p>{price}$</p>
+      <p>{price} $</p>
       <div className={styles.actions}>
         <Link to={`/products/${id}`}>
           <TbListDetails />
         </Link>
-        <button>
+        <button onClick={clickHandler}>
           <TbShoppingBagCheck />
         </button>
       </div>
--- src/constants/list.js
+++ src/constants/list.js
@@ -3,7 +3,7 @@ const categories = [
   { id: 2, type: "Electronics" },
   { id: 3, type: "Jewelery" },
   { id: 4, type: "Men's Clothing" },
-  { id: 5, type: "Woman's Clothing" },
+  { id: 5, type: "Women's Clothing" },
 ];
 
 export { categories };
