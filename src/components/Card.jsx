a29632d0b0ce76f74b5a56522cebe492591473b2
Soudabeh Noorollahi <s.nourollahii@gmail.com>
2025-01-17 11:00:50 +0100

    Align buttons in the card component and update styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
 import { TbShoppingBagCheck } from "react-icons/tb";
+import { MdDeleteOutline } from "react-icons/md";
 
-import { shortenText } from "../helper/helper";
+import { productQuantity, shortenText } from "../helper/helper";
 import { useCart } from "../context/CartContext";
 
 import styles from "./Card.module.css";
@@ -11,10 +12,12 @@ function Card({ data }) {
   const { id, title, image, price } = data;
 
   const [state, dispatch] = useCart();
-  console.log(state);
 
-  const clickHandler = () => {
-    dispatch({ type: "REMOVE_ITEM", payload: data });
+  const quantity = productQuantity(state, id);
+  console.log(quantity);
+
+  const clickHandler = (type) => {
+    dispatch({ type, payload: data });
   };
 
   return (
@@ -26,9 +29,27 @@ function Card({ data }) {
         <Link to={`/products/${id}`}>
           <TbListDetails />
         </Link>
-        <button onClick={clickHandler}>
-          <TbShoppingBagCheck />
-        </button>
+        <div>
+          {quantity === 1 && (
+            <button onClick={() => clickHandler("REMOVE_ITEM")}>
+              <MdDeleteOutline />
+            </button>
+          )}
+
+          {quantity > 1 && (
+            <button onClick={() => clickHandler("DECREASE")}>-</button>
+          )}
+
+          {!!quantity && <span>{quantity}</span>}
+
+          {quantity === 0 ? (
+            <button onClick={() => clickHandler("ADD_ITEM")}>
+              <TbShoppingBagCheck />
+            </button>
+          ) : (
+            <button onClick={() => clickHandler("INCREASE")}>+</button>
+          )}
+        </div>
       </div>
     </div>
   );
